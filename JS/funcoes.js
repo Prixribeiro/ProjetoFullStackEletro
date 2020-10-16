d64a52fe1b416376e430856c7a5ae665760b4b77
= <=>
2020-10-16 01:05:58 -0300

    inclusão de eventos e funções

--- JS/funcoes.js
+++ JS/funcoes.js
@@ -1,3 +1,31 @@
 function exibir_categoria(categoria){
-    alert (categoria)
-} 
+    let elementos = document.getElementsByClassName('box_produto');
+    console.log (elementos);
+    for (var i=0; i<elementos.length; i++){
+        console.log (elementos [i].id);
+            if (categoria == elementos [i].id)
+                elementos [i].style = "display:inline-block";
+            else 
+            elementos [i].style = "display:none";
+    }
+}; 
+
+function exibir_todos() {
+    let elementos = document.getElementsByClassName('box_produto');
+
+        for (var i=0; i<elementos.length; i++){
+        elementos [i].style = "display:inline-block";
+    }
+}; 
+
+function msg_compra (){
+    alert ("Item adicionado ao carrinho!");
+};
+
+function destaque(imagem){
+    console.log(imagem);
+    if (imagem.width == 280)
+        imagem.width = 150;
+    else 
+        imagem.width = 280;
+};
