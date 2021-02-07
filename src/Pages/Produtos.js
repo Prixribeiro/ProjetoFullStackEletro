e7295c339b1a3d6ff8ce0182bf26fe28091a9618
Priscila Ribeiro <priscila@MacBook-Pro-de-Priscila.local>
2021-02-07 18:14:11 -0200

    correção API

--- src/Pages/Produtos.js
+++ src/Pages/Produtos.js
@@ -9,7 +9,7 @@ export default function Produtos(){
     const [produtos, setProdutos] = useState ([]);
   
     useEffect(async () => {
-          const resposta = await fetch("http://localhost:8888/ProjetoFullStackEletro-php/PHP/Api/produto.php");
+          const resposta = await fetch("http://localhost:5000/produtos");
           const dados = await resposta.json();
           setProdutos(dados);
       }, []);
